8c6a7f0fd2b0522ef0dfd2c9c0ddf6a1750df12d
Ivo Rojas <ivo753@gmail.com>
2025-04-05 14:51:12 -0400

    Added preact island, and dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-test-one.netlify.app/"
+  site: "https://astro-test-one.netlify.app/",
+  integrations: [preact()]
 });
